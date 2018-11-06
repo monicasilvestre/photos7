67a05c6012f4b03ea83d166ab932dd613cf3db0b
Monica Silvestre <monicasilvestre@outlook.com>
2018-11-06 19:49:47 +0000

    new angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PhotosComponent } from './photos/photos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
